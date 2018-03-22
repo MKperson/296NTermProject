ac856d418f052f528ef2e221f2e1665208d4f56d
Mike Kvederis <m_kvederis@msn.com>
2018-03-22 13:02:50 -0700

    added developer stuff

---
+++ 296NTermProject/Controllers/DeveloperController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _296NTermProject.Repositorys;
+using Microsoft.AspNetCore.Authorization;
+using _296NTermProject.Models;
+
+namespace _296NTermProject.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        private IDeveloperRepo developerRepo;
+
+        public DeveloperController(IDeveloperRepo repo)
+        {
+            developerRepo = repo;
+        }
+
+        
+
+        public ViewResult Index()
+        {
+            var authors = developerRepo.GetAllDevelopers();
+            return View(authors);
+        }
+
+
+
+        [HttpPost]
+        [Authorize]
+        public RedirectToActionResult Add(string name, DateTime date, int gameId)
+        {
+            developerRepo.Add(new Developer { CompanyName = name, GameID = gameId });
+            return RedirectToAction("Index", "Game");
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            return View("DeveloperEntry", developerRepo.GetDeveloperById(id));
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Edit(String name, int developerid, int gameid)
+        {
+            Developer developer = new Developer
+            {
+                CompanyName = name,
+                DeveloperID = developerid,
+                GameID = gameid
+            };
+
+            developerRepo.Edit(developer);
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult Delete(int id)
+        {
+            developerRepo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+    
